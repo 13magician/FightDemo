11967e72aaf3ce7d6aabed3803d050de07e28ca5
13magician <q631126452@qq.com>
2015-03-09 22:17:06 +0800

    增加一个攻击动画，按X

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {//镜头跟踪角色
+
+    public Transform character;//玩家角色
+    public float xCameraMaxOffset = 1, yCameraMaxOffset = 1;//超过偏移镜头才会自动跟踪
+    public float CamaeraMin = -16.37f, CamaeraMax = 4.36f;//限制镜头X轴的最大最小值
+    public float xSmooth = 2f,ySmooth=2f;//设置镜头平移时的速度
+    //private Transform playerCamera;//用于玩家的镜头
+    private float cameraYoffset = 1.69f;//镜头Y轴和角色Y轴的偏移
+   
+    void Start () {
+        // playerCamera = (GameObject.FindWithTag("MainCamera") as GameObject).GetComponent<Transform>();//获取MainCamera摄像机的transform
+        //character = (GameObject.FindWithTag("Player") as GameObject).GetComponent<Transform>();//获取玩家角色
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+    void LateUpdate()
+    {
+        ResetCamera();//重设镜头
+    }
+    void ResetCamera()//设置镜头的XY跟角色对齐
+    {
+        transform.position = new Vector3(character.position.x, character.position.y + cameraYoffset, transform.position.z);
+        return;//用过度方式会出现类似卡顿现象
+        //Vector3 vt3 = transform.position;//默认保存镜头的向量
+        //if (Mathf.Abs(character.position.x - transform.position.x) > xCameraMaxOffset)//玩家的X位置减去镜头所在的Y位置大于镜头跟随最大值
+        //{
+        //    vt3.x = character.position.x;//获取玩家的X坐标
+        //    vt3.x = Mathf.Lerp(transform.position.x, vt3.x, Time.deltaTime*xSmooth);//设置镜头的X值
+        //}
+        //if (Mathf.Abs(character.position.y - transform.position.y) > yCameraMaxOffset)//玩家的Y位置减去镜头所在的Y位置大于镜头跟随最大值
+        //{
+        //    vt3.y = character.position.y + cameraYoffset;//计算镜头和角色Y轴偏移
+        //    vt3.y = Mathf.Lerp(transform.position.y, vt3.y, Time.deltaTime * ySmooth);//设置镜头的Y值
+        //}
+        //transform.position = vt3;//最终改变镜头的位置
+    }
+}
--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour {//主要玩家控制角色
 
     // Use this for initialization
-    public float maxSpeed=10;//玩家移动速度
-    public float moveForce = 100;//移动时每次增加的力
+    public float moveMaxForce=10;//玩家移动速度
+    public float moveForce = 7;//移动时每次增加的力
     [HideInInspector]//属性不显示在Inspector。虽然不显示，但是仍然会取属性不显示在Inspector面板中的默认值···记得去掉查看/修改
     public Animator anim;
     [HideInInspector]
@@ -16,7 +16,8 @@ public class PlayerControl : MonoBehaviour {
     void Start () {
         anim = GetComponent<Animator>();
         rigid =GetComponent<Rigidbody2D>();
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -32,7 +33,21 @@ public class PlayerControl : MonoBehaviour {
             if (rightSide) Flip();
         } 
         anim.SetBool("run", rightArrow || leftArrow);//按下左右任意一个都true
+     
+        if (Input.GetKeyDown(KeyCode.X) && !IsName("attack2"))//发动攻击2动画
+        {
+            anim.SetTrigger("attack2");
+        }
     }
+    bool IsName(string name)
+    {
+        return anim.GetCurrentAnimatorStateInfo(0).IsName("attack2");
+    }
+    void LateUpdate()
+    {
+       
+    }
+
     void FixedUpdate()
     {
         if(leftArrow)
@@ -42,8 +57,12 @@ public class PlayerControl : MonoBehaviour {
         {
             rigid.AddForce(new Vector2(moveForce, 0));
         }
+        if(Mathf.Abs( rigid.velocity.x)>moveMaxForce)//如果X轴力大于限制的速度。就设为最大速度
+        {
+            rigid.velocity = new Vector2(Mathf.Sign(rigid.velocity.x) * moveMaxForce, rigid.velocity.y);
+        }
     }
-    void Flip()
+    void Flip()//面向反转函数
     {
         Vector3 vt3 = transform.localScale;
         vt3.x *= -1;
