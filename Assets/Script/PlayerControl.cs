7c43ffecc3ec17dcd65b5f7c01483506a1a72186
13magician <q631126452@qq.com>
2015-03-09 17:13:16 +0800

    完成左右动画（没移动）

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,28 +4,31 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
     // Use this for initialization
-    [HideInInspector]//属性不显示在Inspector
+    [HideInInspector]//属性不显示在Inspector。虽然不显示，但是仍然会取属性不显示在Inspector面板中的默认值···记得去掉查看/修改
     public Animator anim;
     [HideInInspector]
     public bool rightArrow, leftArrow;//玩家是否按下左右键
     [HideInInspector]
-    public bool rightSide;//控制角色的面相
+    public bool rightSide=true;//控制角色的面相,.默认是右边
 	void Start () {
         anim = GetComponent<Animator>();
+        Debug.Log(rightSide);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rightArrow = Input.GetKey(KeyCode.RightArrow);//玩家是否按下→方向键
-            leftArrow = Input.GetKey(KeyCode.LeftArrow);//玩家是否按下←方向键
-        anim.SetBool("run", rightArrow || leftArrow);//按下左右任意一个都true
-        if(rightArrow && !rightSide)//如果按下→键以及面向不是右边
+        leftArrow = rightArrow = false;//默认是false··无视这点效率了
+        if (Input.GetKey(KeyCode.RightArrow))//玩家是否按下→方向键
         {
-            Flip();//调用反转函数
-        }else if(leftArrow && rightSide) //如果按下←键以及面向是右边
-        {
-            Flip();
+            rightArrow = true;
+            if(!rightSide) Flip();//调用反转函数
         }
+        else if (Input.GetKey(KeyCode.LeftArrow)) //玩家是否按下←方向键
+        {
+            leftArrow = true;
+            if (rightSide) Flip();
+        } 
+        anim.SetBool("run", rightArrow || leftArrow);//按下左右任意一个都true
     }
     void FixedUpdate()
     {
@@ -33,10 +36,10 @@ public class PlayerControl : MonoBehaviour {
     }
     void Flip()
     {
-        rightSide = !rightSide;//设置角色面相相反
-        Vector3 vt3 = transform.localScale;
+        Transform parent = transform.parent;//获取父物体
+        Vector3 vt3 = parent.localScale;
         vt3.x *= -1;
-        transform.localScale = vt3;
-        Debug.Log(transform.localScale);
+        parent.localScale = vt3;//修改父物体x缩放为反方向
+        rightSide = !rightSide;//设置角色面相相反
     }
 }
