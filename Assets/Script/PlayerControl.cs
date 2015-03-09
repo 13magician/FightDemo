093c4d094288f44d468c4861668b1d4761d88ce0
13magician <q631126452@qq.com>
2015-03-09 18:59:22 +0800

    角色的左右移动（基于物理力）

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,15 +4,18 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
     // Use this for initialization
+    public float maxSpeed=10;//玩家移动速度
+    public float moveForce = 100;//移动时每次增加的力
     [HideInInspector]//属性不显示在Inspector。虽然不显示，但是仍然会取属性不显示在Inspector面板中的默认值···记得去掉查看/修改
     public Animator anim;
     [HideInInspector]
     public bool rightArrow, leftArrow;//玩家是否按下左右键
     [HideInInspector]
     public bool rightSide=true;//控制角色的面相,.默认是右边
-	void Start () {
+    private Rigidbody2D rigid;//角色的刚体组件
+    void Start () {
         anim = GetComponent<Animator>();
-        Debug.Log(rightSide);
+        rigid =GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -32,14 +35,19 @@ public class PlayerControl : MonoBehaviour {
     }
     void FixedUpdate()
     {
-
+        if(leftArrow)
+        {
+            rigid.AddForce(new Vector2(-moveForce,0));//给角色添加力
+        }else if(rightArrow)
+        {
+            rigid.AddForce(new Vector2(moveForce, 0));
+        }
     }
     void Flip()
     {
-        Transform parent = transform.parent;//获取父物体
-        Vector3 vt3 = parent.localScale;
+        Vector3 vt3 = transform.localScale;
         vt3.x *= -1;
-        parent.localScale = vt3;//修改父物体x缩放为反方向
+        transform.localScale = vt3;//修改父物体x缩放为反方向
         rightSide = !rightSide;//设置角色面相相反
     }
 }
